4f83266a9c9166f8b5d61366465c8bb2e185f057
amranhakim10 <amranhakim9@gmail.com>
2023-02-27 22:57:57 +0700

    Initial commit

--- login.php
+++ login.php
@@ -20,6 +20,27 @@ include "include/login/head.php";
           <p>Happy to see you again!</p>
 
           <!-- Alert -->
+          <div class="alert alert-secondary alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <i class="ik ik-x"></i>
+            </button>
+          </div>
+
+          <div class="alert alert-info alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <i class="ik ik-x"></i>
+            </button>
+          </div>
+
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <i class="ik ik-x"></i>
+            </button>
+          </div>
+
           <div class="alert alert-warning alert-dismissible fade show" role="alert">
             <strong>Holy guacamole!</strong> You should check in on some of those fields below.
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
